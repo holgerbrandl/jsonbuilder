58f5e53cd36acb80f4b351789de82eb9c8d06469
Holger Brandl <holgerbrandl@users.noreply.github.com>
2020-11-14 21:08:39 +0100

    Started test suite

---
+++ src/test/kotlin/Tests.kt
@@ -0,0 +1,42 @@
+import com.systema.analytics.es.misc.json
+import io.kotlintest.shouldBe
+import org.junit.Test
+
+
+class BuilderTests{
+
+    @Test
+    fun `it should preserver order`(){
+        json{
+            "name" to 1
+            "type" to 1
+            "queue" to 1
+            "component" to 1
+        }.toString(3).apply {
+            println()
+            toString() shouldBe """
+                {
+                   "name": 1,
+                   "type": 1,
+                   "queue": 1
+                   "component": 1,
+                }
+            """.trimIndent()
+        }
+    }
+
+    // https://github.com/holgerbrandl/jsonbuilder/issues/1
+    @Test
+    fun `it should support root arrays`(){
+        json {
+             null to arrayOf(
+                 json{ "foo" to "bar"},
+                 json{ "foo" to "bar"}
+             )
+        }.println()
+    }
+}
+
+internal fun Any.println() {
+    println(toString())
+}
