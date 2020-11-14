2fdb5dcb602c9001e3ffb21584b1b8a7ec1c880d
Holger Brandl <holgerbrandl@users.noreply.github.com>
2020-11-14 21:09:09 +0100

    Started fix of #1

--- src/main/kotlin/com/github/holgerbrandl/jsonbuilder/JsonUtil.kt
+++ src/main/kotlin/com/github/holgerbrandl/jsonbuilder/JsonUtil.kt
@@ -31,4 +31,24 @@ class JsonObjectBuilder {
 
         deque.peek().put(this, wrapped)
     }
+
+//    infix fun <T> String?.to(value: T) {
+//        if (this == null) {
+//            val wrapped = when (value) {
+//                is Array<*> -> deque.peek().put(this, JSONArray().apply { value.forEach { put(it) } })
+//                else -> TODO("not support. Please file an issue if you that this is an error of API")
+//            }
+//
+//        } else {
+//            // wrap value into json block if it is a lambda
+//            val wrapped = when (value) {
+//                is Function0<*> -> json { value.invoke() }
+//                is Array<*> -> JSONArray().apply { value.forEach { put(it) } }
+//                else -> value
+//            }
+//
+//            deque.peek().put(this, wrapped)
+//
+//        }
+//    }
 }
